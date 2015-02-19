60a3cd03141cb6b76f7f0e671f4b7f1d308056d4
Alex Shadrin <alex@shdlabs.com>
2015-02-19 12:51:21 +0200

    riemann-dash installed with chef_gem

--- recipes/riemann-dash.rb
+++ recipes/riemann-dash.rb
@@ -1,8 +1,5 @@
-include_recipe 'build-essential'
-include_recipe 'rvm::system'
 include_recipe 'runit'
 
-gem_package 'riemann-dash' do
+chef_gem 'riemann-dash' do
   action      :install
-  gem_binary  'gem'
 end
