7be43e7c1ba90a39b73fbad2fb21413d4d28a53b
Alex Shadrin <alex.shadrin@teamaol.com>
2015-08-16 10:17:27 +0300

    version update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alex@fewbytes.com'
 license          'Apache 2.0'
 description      'Installs/Configures riemann'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 supports 'ubuntu'
 supports 'debian'
