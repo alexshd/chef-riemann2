8b0f1ac0bdd69dbd2ef9da29f41bdc90c24e5060
Alex Shadrin <alex.shadrin@teamaol.com>
2015-10-03 16:51:21 +0300

    updates

--- recipes/dash.rb
+++ recipes/dash.rb
@@ -3,25 +3,27 @@
 # Recipe::dash
 #
 include_recipe 'runit'
+include_recipe 'riemann2::default'
 
 chef_gem 'riemann-dash' do
   action :install
 end
 
 directory '/opt/riemann2/dash' do
-  owner 'root'
-  group 'root'
+  owner node.riemann.system.user
+  group node.riemann.system.group
   mode '0755'
   recursive true
   action :create
 end
 
-
-template '/opt/riemann2/dash/config.rb' do
-  source 'config.rb.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
+%w( config.rb config.json).each do |config_file|
+  template "/opt/riemann2/dash/#{config_file}" do
+    source "#{config_file}.erb"
+    owner node.riemann.system.user
+    group node.riemann.system.group
+    mode '0644'
+  end
 end
 
 runit_service 'riemann-dash'
