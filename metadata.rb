d0f57ffdc76e68a70b197722c7ba5a62ffaf89ea
Alex Shadrin <alex.shadrin@teamaol.com>
2015-10-04 13:23:04 +0300

    version update

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,15 @@ name             'riemann2'
 maintainer       'Alex Shadrin.'
 maintainer_email 'alex@shdlabs.com'
 license          'Apache 2.0'
-description      'Installs/Configures riemann'
+description      'Installs/Configures riemann, riemann-dash and rienann-tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/alexshd/chef-riemann2.git'
-version          '0.3.1'
+version          '0.3.3'
+
+recipe 'riemann2::default', "creating user and group, included in the `server` and `dash` recipes"
+recipe 'riemann2::server',  "installs riemann server, setting 2 config files => default ( riemann.conf ) and user.conf, for custumisations"
+recipe 'riemann2::dash',    "install `riemann-dash` with `config.rb` and `config.json` from templates"
+recipe 'riemann2::tools',   "installs `riemann-tools`"
 
 supports 'ubuntu'
 supports 'debian'
