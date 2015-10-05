7eebfaf5a81d52e1a560a2525ef076caf5dc3058
Alex Shadrin <alex.shadrin@teamaol.com>
2015-10-06 00:29:50 +0300

    more attributes, better example

--- resources/check.rb
+++ resources/check.rb
@@ -6,9 +6,11 @@ This creates and destroy the riemann-health service.
 
 @section Examples
 
-    # An example of my awesome service
+    # riemann-health 
     riemann2::check "health" do
       action :create
+      server node['riemann']['server']['ip']
+      port   '5555'
     end
 #>
 =end
@@ -17,6 +19,6 @@ This creates and destroy the riemann-health service.
 actions :create, :start, :destroy
 
 attribute :check_name, name_attribute: true,  kind_of: String
-# attribute :server,     default: 'localhost',  kind_of: String, required: true
-# attribute :port,       default: 5555,         kind_of: Fixnum
-# attribute :tags,       kind_of: Array
+attribute :server,     default: 'localhost',  kind_of: String, required: true
+attribute :port,       default: '5555',         kind_of: String
+attribute :tags,       kind_of: Array
